143c2b1a2ba5067685bb00a5ad7b026574a908fb
toplan <toplan710@gmail.com>
2016-08-06 00:22:39 +0800

    update

--- list-diff/diff.js
+++ list-diff/diff.js
@@ -17,26 +17,24 @@ var DELETION = 0
 var INSERTION = 1
 var SUBSTITUTION = 2
 
-var distance = []
+var map = []
 var actions = []
-var logs = []
 
-function Action (index, type, item, distance) {
+function Action(index, type, item) {
   if (!_.isObject(this)) {
     return new Action(index, type, item)
   }
   this.index = index
   this.type = type
   this.item = item
-  this._distance = distance 
 }
 
-function deletion (oldPos, newPos) {
-  return distance[oldPos - 1][newPos] + 1
+function deletion(oldPos, newPos) {
+  return map[oldPos - 1][newPos] + 1
 }
 
-function insertion (oldPos, newPos) {
-  return distance[oldPos][newPos - 1] + 1
+function insertion(oldPos, newPos) {
+  return map[oldPos][newPos - 1] + 1
 }
 
 function substitution(oldPos, newPos, cost) {
@@ -46,31 +44,35 @@ function substitution(oldPos, newPos, cost) {
   if (cost !== 0 && cost !== 1) {
     throw new Error("Cost param must be 0 or 1.")
   }
-  return distance[oldPos - 1][newPos - 1] + cost
+  return map[oldPos - 1][newPos - 1] + cost
 }
 
-function initDistanceData (oldList, newList) {
+function initDistanceData(oldList, newList) {
   var oldLen = oldList.length
   var newLen = newList.length
+
+  map = []
+  actions = []
 
   for (var i = 0; i <= oldLen; i++) {
-    distance[i] = [i]
+    map[i] = [i]
   }
   for (var i = 0; i <= newLen; i++) {
-    distance[0][i] = i
+    map[0][i] = i
   }
 }
 
-function computeDistance (oldList, newList, key) {
+function computeDistance(oldList, newList, key) {
   var oldLen = oldList.length
   var newLen = newList.length
-  var preMinDistance = 0
 
   for (var i = 1; i <= oldLen; i++) {
     for (var j = 1; j <= newLen; j++) {
       var distanceOfDeletion = deletion(i, j)
       var distanceOfInsertion = insertion(i, j)
-      var distanceOfSubstitution = substitution(i, j, function () {
+
+      var costFn
+      var distanceOfSubstitution = substitution(i, j, costFn = function () {
         var oldItem = oldList[i - 1]
         var newItem = newList[j - 1]
         if (!key || !(key in oldItem) || !(key in newItem)) {
@@ -83,38 +85,56 @@ function computeDistance (oldList, newList, key) {
       })
 
       var min = Math.min(distanceOfDeletion, distanceOfInsertion, distanceOfSubstitution)
-      distance[i][j] = min
-      updateActions(i, j, min, preMinDistance, function (push) {
-        preMinDistance = min
-        if (push) {
-          switch (min) {
-            case distanceOfDeletion: actions.push(new Action(i - 1, DELETION, null, min)); break;
-            case distanceOfInsertion: actions.push(new Action(i - 1, INSERTION, newList[j - 1], min)); break;
-            case distanceOfSubstitution: actions.push(new Action(i - 1, SUBSTITUTION, newList[j - 1], min)); break;
-            default: break;
-          }
+      map[i][j] = min
+
+      //
+      if (min && i === j) {
+        switch (min) {
+          case distanceOfDeletion:
+            actions.push(new Action(i - 1, DELETION)); break;
+          case distanceOfInsertion:
+            actions.push(new Action(i - 1, INSERTION, newList[j - 1])); break;
+          case distanceOfSubstitution:
+            if (costFn()) {
+              actions.push(new Action(i - 1, SUBSTITUTION, newList[j - 1]));
+            }
+            break;
+          default: break;
         }
-      })
+      }
+      if (i === oldLen && j > i) {
+        switch (min) {
+          case distanceOfInsertion:
+            actions.push(new Action(i - 1, INSERTION, newList[j - 1])); break;
+          case distanceOfSubstitution:
+            if (costFn()) {
+              actions.push(new Action(i - 1, SUBSTITUTION, newList[j - 1]));
+            }
+            break;
+          default:
+            break;
+        }
+      }
+      if (j === newLen && i > j) {
+        switch (min) {
+          case distanceOfDeletion:
+            actions.push(new Action(i - 1, DELETION)); break;
+          case distanceOfSubstitution:
+            if (costFn()) {
+              actions.push(new Action(i - 1, SUBSTITUTION, newList[j - 1]));
+            }
+            break;
+          default: break;
+        }
+      }
     }
   }
 
-  return getMinDistance()
+  return getDistance(map)
 }
 
-function updateActions (oldPos, newPos, minDistance, preMinDistance, cb) {
-  if (minDistance && (minDistance - preMinDistance === 1 || minDistance == preMinDistance)) {
-    actions.forEach(function (action, index) {
-      if (action._distance === minDistance) {
-        actions.splice(index, 1)
-      }
-    })
-    return cb(true)
-  }
-  cb(false)
-}
-
-function getMinDistance() {
-  var array = distance[distance.length - 1]
+function getDistance(map) {
+  var array = map[map.length - 1]
   return array[array.length - 1]
 }
 
@@ -125,22 +145,16 @@ function diff(oldList, newList, key) {
   if (!_.isArray(newList)) {
     newList = [newList]
   }
+
   oldList = oldList.filter(_.isObject)
   newList = newList.filter(_.isObject)
 
   initDistanceData(oldList, newList)
-
-  var minDistance = computeDistance(oldList, newList, key)
-
-  actions = actions.filter(function (action) {
-    var isPass = action._distance <= minDistance
-    delete action._distance
-    return isPass
-  })
+  var distance = computeDistance(oldList, newList, key)
 
   return {
-    minDistance: minDistance,
     distance: distance,
+    map: map,
     actions: actions
   }
 }
--- list-diff/test.js
+++ list-diff/test.js
@@ -1,35 +1,25 @@
 var diff = require("./diff.js")
 
-var oldList = [
-  {
-    key: "a",
-  },
-  {
-    key: "b",
-  },
-  {
-    key: "d"
-  }
-]
+if (true) {
+  var old0 = [{key: "a"}, {key: "b"}, {key: "d"}]
+  var new0 = [{key: "e"}, {key: "b"}, {key: "m"}, {key: "d"}]
 
-var newList = [
-  {
-    key: "e"
-  },
-  {
-    key: "b"
-  },
-  {
-    key: "m"
-  },
-  {
-    key: "d"
-  }
-]
+  var result = diff(old0, new0, "key")
+  console.log(result)
 
-var result = diff(oldList, newList, "key")
-console.log(result)
+  result.actions.forEach(function (action) {
+    console.log(action)
+  })
+}
 
-result.actions.forEach(function (action) {
-  console.log(action)
-})
+if (true) {
+  var old1 = [{key: "s"}, {key: "i"}, {key: "e"}, {key: "j"}]
+  var new1 = [{key: "m"}, {key: "i"}, {key: "e"}, {key: "k"}, {key: "k"}]
+
+  var result = diff(old1, new1, "key")
+  console.log(result)
+
+  result.actions.forEach(function (action) {
+    console.log(action)
+  })
+}
